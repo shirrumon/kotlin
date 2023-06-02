bc986927097695dbe4b4c92d2406569806730811
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-06-02 18:06:11 +0200

    [FE 1.0] Prohibit implicit Java actualization in K1
    ^KT-58545 Fixed
    Review: https://jetbrains.team/p/kt/reviews/10561
    
    It's not yet supported in K2 KT-59213
    
    Related tests:
    - ApiTest.testStdlib
    - RuntimePublicAPITest.kotlinStdlibRuntimeMerged
    - KotlinProjectViewTestGenerated.test_Arrays (in Kotlin plugin)

---
+++ compiler/testData/diagnostics/tests/multiplatform/java/implicitJavaActualizationAllowed.fir.kt
@@ -0,0 +1,19 @@
+// WITH_STDLIB
+
+// MODULE: m1-common
+// FILE: common.kt
+
+import kotlin.jvm.ImplicitlyActualizedByNonKotlin
+
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>@OptIn(ImplicitlyActualizedByNonKotlin::class)
+expect class <!PACKAGE_OR_CLASSIFIER_REDECLARATION!>Foo<!>() {
+    fun foo()
+}<!>
+
+// MODULE: m2-jvm()()(m1-common)
+// FILE: Foo.java
+
+public class Foo {
+    public void foo() {
+    }
+}
