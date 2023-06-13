4cc70f84e394e853cd0e9128a259d033bf50467f
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-06-13 13:10:32 +0200

    [FE] Cover "implicit actualization"/"actual fake-overrides" KT-58544 with regression tests
    Review: https://jetbrains.team/p/kt/reviews/11039/timeline

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendMPPDiagnosticsWithLightTreeTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendMPPDiagnosticsWithLightTreeTestGenerated.java
@@ -21,6 +21,18 @@ import java.util.regex.Pattern;
 @TestDataPath("$PROJECT_ROOT")
 public class FirOldFrontendMPPDiagnosticsWithLightTreeTestGenerated extends AbstractFirLightTreeWithActualizerDiagnosticsTest {
     @Test
+    @TestMetadata("actualFakeOverride.kt")
+    public void testActualFakeOverride() throws Exception {
+        runTest("compiler/testData/diagnostics/tests/multiplatform/actualFakeOverride.kt");
+    }
+
+    @Test
+    @TestMetadata("actualFakeOverrideVirtual.kt")
+    public void testActualFakeOverrideVirtual() throws Exception {
+        runTest("compiler/testData/diagnostics/tests/multiplatform/actualFakeOverrideVirtual.kt");
+    }
+
+    @Test
     public void testAllFilesPresentInMultiplatform() throws Exception {
         KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/diagnostics/tests/multiplatform"), Pattern.compile("^(.*)\\.kts?$"), Pattern.compile("^(.+)\\.(reversed|fir|ll)\\.kts?$"), TargetBackend.JVM_IR, true);
     }
@@ -83,6 +95,18 @@ public class FirOldFrontendMPPDiagnosticsWithLightTreeTestGenerated extends Abst
     @TestMetadata("implFakeOverride.kt")
     public void testImplFakeOverride() throws Exception {
         runTest("compiler/testData/diagnostics/tests/multiplatform/implFakeOverride.kt");
+    }
+
+    @Test
+    @TestMetadata("implicitActualFakeOverride_AbstractMap.kt")
+    public void testImplicitActualFakeOverride_AbstractMap() throws Exception {
+        runTest("compiler/testData/diagnostics/tests/multiplatform/implicitActualFakeOverride_AbstractMap.kt");
+    }
+
+    @Test
+    @TestMetadata("implicitActualFakeOverride_simple.kt")
+    public void testImplicitActualFakeOverride_simple() throws Exception {
+        runTest("compiler/testData/diagnostics/tests/multiplatform/implicitActualFakeOverride_simple.kt");
     }
 
     @Test
