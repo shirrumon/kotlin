de35c30a53138cb5747a8b7d5cc0dc5aae152201
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-06-28 16:42:26 +0200

    [FE] Convert specific diagnostic for actual function with default arguments into a common incompatibility
    ^KT-59665 Fixed
    Review: https://jetbrains.team/p/kt/reviews/11039/timeline
    
    It's better to have this logic in common place
    (AbstractExpectActualCompatibilityChecker) to avoid missing compilation
    errors in the future
    
    This commit fixes:
    1. Missing compilation error for actual function with default arguments
       for 'actual typealias' KT-59665
    2. Missing compilation error for actual function with default arguments
       for actual fake-override KT-59665
    
    Alternative solution for KT-59665 is to create a special checker.
    
    "incompatibility" vs "special checker":
    
    Arguments for common incompatibility:
    - What if we had a rule that expect and actual default params must
      match? If so then it certainly would be an incompatibility.
    - Technically, we do the matching of expect and actual params (because
      we allow default params in common ancestors of expect and actual
      declarations).
    - It's hard to check that the actual definition doesn't use default
      params because `ExpectedActualResolver.findActualForExpected` filters
      out fake-overrides and doesn't return them. It's not clear logic for
      me, that I'm afraid to touch.
      implicitActualFakeOverride_AbstractMap.kt test breaks if you drop this
      weird logic
    
    Arguments against common incompatibility:
    - Although we match expect and actual declarations to allow default
      params in common ancestors of expect and actual declarations, it's
      still can be considered that we check that the actual declaration
      doesn't have default params. And it doesn't feel right that we check
      correctness of the actual declaration in expect-actual matcher.
    - It may change the rules of expect actual matching

--- compiler/testData/diagnostics/tests/multiplatform/defaultArguments/expectedInheritsDefaultArguments.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/defaultArguments/expectedInheritsDefaultArguments.fir.kt
@@ -27,7 +27,7 @@ fun test(foo: Foo, bar: Bar) {
 // MODULE: m2-jvm()()(m1-common)
 // FILE: jvm.kt
 
-actual class Bar : Foo {
+actual class <!NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS!>Bar<!> : Foo {
     actual override fun ok(x: Int, y: String) {}
 
     actual override fun failX(<!ACTUAL_FUNCTION_WITH_DEFAULT_ARGUMENTS!>x: Int = 0<!>, y: String) {}
