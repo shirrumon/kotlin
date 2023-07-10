948859c865d1c955dae96c151cccf326080d44a1
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-07-10 11:23:25 +0200

    [FE] Stop ignoring ABSTRACT_MEMBER_NOT_IMPLEMENTED for expect classes
    ^KT-59739 Fixed
    Review: https://jetbrains.team/p/kt/reviews/11038/timeline

--- compiler/testData/diagnostics/tests/multiplatform/headerClass/extendExpectedClassWithAbstractMember.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/headerClass/extendExpectedClassWithAbstractMember.fir.kt
@@ -4,9 +4,9 @@
 expect abstract class BaseA() {
     abstract fun foo()
 }
-expect open class BaseAImpl() : BaseA
+expect open <!ABSTRACT_CLASS_MEMBER_NOT_IMPLEMENTED!>class BaseAImpl<!>() : BaseA
 
-class DerivedA1 : BaseAImpl()
+<!ABSTRACT_CLASS_MEMBER_NOT_IMPLEMENTED!>class DerivedA1<!> : BaseAImpl()
 class DerivedA2 : BaseAImpl() {
     override fun foo() = super.<!ABSTRACT_SUPER_CALL!>foo<!>()
 }
@@ -16,9 +16,9 @@ class DerivedA2 : BaseAImpl() {
 expect interface BaseB {
     fun foo()
 }
-expect open class BaseBImpl() : BaseB
+expect open <!ABSTRACT_MEMBER_NOT_IMPLEMENTED!>class BaseBImpl<!>() : BaseB
 
-class DerivedB1 : BaseBImpl()
+<!ABSTRACT_MEMBER_NOT_IMPLEMENTED!>class DerivedB1<!> : BaseBImpl()
 class DerivedB2 : BaseBImpl() {
     override fun foo() = super.<!ABSTRACT_SUPER_CALL!>foo<!>()
 }
@@ -30,7 +30,7 @@ expect interface BaseC {
 }
 expect abstract class BaseCImpl() : BaseC
 
-class DerivedC1 : BaseCImpl()
+<!ABSTRACT_MEMBER_NOT_IMPLEMENTED!>class DerivedC1<!> : BaseCImpl()
 class DerivedC2 : BaseCImpl() {
     override fun foo() = super.<!ABSTRACT_SUPER_CALL!>foo<!>()
 }
@@ -58,4 +58,14 @@ sealed class BaseEImpl() : BaseE {
 expect interface BaseF {
     fun foo()
 }
-expect class BaseFImpl() : BaseF
+expect <!ABSTRACT_MEMBER_NOT_IMPLEMENTED!>class BaseFImpl<!>() : BaseF
+
+
+
+expect abstract class BaseG() {
+    abstract fun foo()
+}
+expect open class BaseGImpl() : BaseG {
+    override fun foo()
+}
+class DerivedG1 : BaseGImpl()
