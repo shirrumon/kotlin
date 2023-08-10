ede697486952674f31b3240b17700748ba2141ca
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2023-08-10 18:09:18 +0200

    [K/N] Remove code for deprecated targets from runtime ^KT-59008

--- kotlin-native/runtime/src/std_alloc/cpp/ObjectAlloc.cpp
+++ kotlin-native/runtime/src/std_alloc/cpp/ObjectAlloc.cpp
@@ -5,34 +5,17 @@
 
 #include "ObjectAlloc.hpp"
 
-#ifndef KONAN_NO_THREADS
 #include <atomic>
-#endif
 
 #include "Memory.h"
 
-#if KONAN_INTERNAL_DLMALLOC
-extern "C" void* dlcalloc(size_t, size_t);
-extern "C" void dlfree(void*);
-
-#define callocImpl dlcalloc
-#define freeImpl dlfree
-#else
 #include <cstdlib>
-
-#define callocImpl ::calloc
-#define freeImpl ::free
-#endif
 
 using namespace kotlin;
 
 namespace {
 
-#ifndef KONAN_NO_THREADS
 std::atomic<size_t> allocatedBytesCounter = 0;
-#else
-size_t allocatedBytesCounter = 0;
-#endif
 
 } // namespace
 
@@ -40,25 +23,16 @@ void kotlin::initObjectPool() noexcept {}
 
 void* kotlin::allocateInObjectPool(size_t size) noexcept {
     // TODO: Check that alignment to kObjectAlignment is satisfied.
-    void* result = callocImpl(1, size);
-#ifndef KONAN_NO_THREADS
+    void* result = calloc(1, size);
     auto newSize = allocatedBytesCounter.fetch_add(size, std::memory_order_relaxed);
     newSize += size;
-#else
-    allocatedBytesCounter += size;
-    auto newSize = allocatedBytesCounter;
-#endif
     OnMemoryAllocation(newSize);
     return result;
 }
 
 void kotlin::freeInObjectPool(void* ptr, size_t size) noexcept {
-#ifndef KONAN_NO_THREADS
     allocatedBytesCounter.fetch_sub(size, std::memory_order_relaxed);
-#else
-    allocatedBytesCounter -= size;
-#endif
-    freeImpl(ptr);
+    free(ptr);
 }
 
 void kotlin::compactObjectPoolInCurrentThread() noexcept {}
@@ -66,9 +40,5 @@ void kotlin::compactObjectPoolInCurrentThread() noexcept {}
 void kotlin::compactObjectPoolInMainThread() noexcept {}
 
 size_t kotlin::allocatedBytes() noexcept {
-#ifndef KONAN_NO_THREADS
     return allocatedBytesCounter.load();
-#else
-    return allocatedBytesCounter;
-#endif
 }
