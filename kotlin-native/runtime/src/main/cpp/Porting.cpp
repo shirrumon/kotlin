2c7712eb451ac2842026674fa8aaf9a6938b9fc9
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2023-08-24 19:09:43 +0200

    [K/N] Move memmem from Porting.h to KString.cpp ^KT-59008

--- kotlin-native/runtime/src/main/cpp/Porting.cpp
+++ kotlin-native/runtime/src/main/cpp/Porting.cpp
@@ -252,20 +252,4 @@ NO_EXTERNAL_CALLS_CHECK int currentThreadId() {
 #endif
 }
 
-// String/byte operations.
-// memcpy/memmove are not here intentionally, as frequently implemented/optimized
-// by C compiler.
-void* memmem(const void *big, size_t bigLen, const void *little, size_t littleLen) {
-#if KONAN_NO_MEMMEM
-  for (size_t i = 0; i + littleLen <= bigLen; ++i) {
-    void* pos = ((char*)big) + i;
-    if (::memcmp(little, pos, littleLen) == 0) return pos;
-  }
-  return nullptr;
-#else
-  return ::memmem(big, bigLen, little, littleLen);
-#endif
-
-}
-
 }  // namespace konan
--- kotlin-native/runtime/src/main/cpp/Porting.h
+++ kotlin-native/runtime/src/main/cpp/Porting.h
@@ -40,11 +40,6 @@ void onThreadExit(void (*destructor)(void*), void* destructorParameter);
 bool isOnThreadExitNotSetOrAlreadyStarted();
 int currentThreadId();
 
-// String/byte operations.
-// memcpy/memmove/memcmp are not here intentionally, as frequently implemented/optimized
-// by C compiler.
-void* memmem(const void *big, size_t bigLen, const void *little, size_t littleLen);
-
 }  // namespace konan
 
 #endif  // RUNTIME_PORTING_H
