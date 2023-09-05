4a2295fce541b27a9cb4b274269ca3222c51081f
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-09-05 18:15:37 +0200

    KMP: Put "prohibit actual typealias to special annotations" under the flag
    ^KT-61668 Fixed
    
    Review: https://jetbrains.team/p/kt/reviews/12062/timeline

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -286,6 +286,7 @@ enum class LanguageFeature(
     ProhibitActualTypealiasIfExpectHasDefaultParams(KOTLIN_1_9, kind = BUG_FIX), // KT-57614
     ProhibitActualTypealiasToNothingAndNullable(KOTLIN_1_9, kind = BUG_FIX), // KT-60650
     ProhibitExpectActualOptInAnnotations(KOTLIN_1_9, kind = BUG_FIX), // KT-58554
+    ProhibitActualTypealiasToSpecialAnnotations(KOTLIN_1_9, kind = BUG_FIX), // KT-58554
     ProhibitExpectActualTailrecOrExternal(KOTLIN_1_9, kind = BUG_FIX), // KT-58536
 
     // End of 1.* language features --------------------------------------------------
