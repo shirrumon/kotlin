44fdc8085a0b599a7f0ebc9cd456e3661c116a10
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-09-05 16:53:22 +0200

    4/7 KMP: Put "prohbit actual typealias if expect has default params" under the flag
    ^KT-61668 Fixed

---
+++ compiler/testData/diagnostics/tests/multiplatform/defaultArguments/methodDefaultArgsViaActualTypealias_oldLanguageVersion.fir.kt
@@ -0,0 +1,26 @@
+// !DIAGNOSTICS: -ACTUAL_WITHOUT_EXPECT
+// LANGUAGE: -ProhibitActualTypealiasIfExpectHasDefaultParams
+// MODULE: m1-common
+// FILE: common.kt
+expect class A {
+    fun foo(p1: String = "common", p2: String = "common", p3: String)
+}
+
+<!INCOMPATIBLE_MATCHING{JVM}!>expect open class WithIncompatibility {
+    fun foo(p: String = "common")
+}<!>
+
+// MODULE: m2-jvm()()(m1-common)
+// FILE: jvm.kt
+
+class AImpl {
+    fun foo(p1: String = "impl", p2: String = "impl", p3: String) {}
+}
+
+actual typealias <!NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS!>A<!> = AImpl
+
+class WithIncompatibilityImpl {
+    fun foo(p: String) {}
+}
+
+actual typealias WithIncompatibility = WithIncompatibilityImpl
