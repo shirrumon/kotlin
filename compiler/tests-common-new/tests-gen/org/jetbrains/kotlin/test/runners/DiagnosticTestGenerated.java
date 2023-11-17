5d90e1750123a61564f186359f9b6a95fe4ff19b
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-11-17 11:39:24 +0100

    [FIR, IR] Convert FirDefaultArgumentsInExpectActualizedByFakeOverrideChecker into ExpectActualCheckingCompatibility
    FirDefaultArgumentsInExpectActualizedByFakeOverrideChecker is an adhoc
    checker which can be converted to ExpectActualCheckingCompatibility to
    reuse common expect-actual checking infrastructure.
    
    ^KT-62913 Fixed
    Review: https://jetbrains.team/p/kt/reviews/13094/timeline
    
    Tests that were broken by one of my previous commits are now fixed:
    - actualFakeOverride_paramsAreCompatibleViaSharedMethodWithDefaultParams.kt
    - inheritedJavaMembers.kt
    
    DEFAULT_ARGUMENTS_IN_EXPECT_ACTUALIZED_BY_FAKE_OVERRIDE diagnostic
    disappeared in delegation.fir.kt because only one
    AbstractExpectActualChecker incompatibility can be reported at a time
    (DEFAULT_ARGUMENTS_IN_EXPECT_ACTUALIZED_BY_FAKE_OVERRIDE is now reported
    not by adhoc checker but by common AbstractExpectActualChecker). It
    would be nice to report both of them, but it's a separate issue KT-62631
    
    delegation2 test makes sure that
    DEFAULT_ARGUMENTS_IN_EXPECT_ACTUALIZED_BY_FAKE_OVERRIDE is reported when
    NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS is fixed

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/DiagnosticTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/DiagnosticTestGenerated.java
@@ -24865,6 +24865,12 @@ public class DiagnosticTestGenerated extends AbstractDiagnosticTest {
                     }
 
                     @Test
+                    @TestMetadata("delegation2.kt")
+                    public void testDelegation2() throws Exception {
+                        runTest("compiler/testData/diagnostics/tests/multiplatform/defaultArguments/methodDefaultArgsViaActualFakeOverride/delegation2.kt");
+                    }
+
+                    @Test
                     @TestMetadata("disabledFeature.kt")
                     public void testDisabledFeature() throws Exception {
                         runTest("compiler/testData/diagnostics/tests/multiplatform/defaultArguments/methodDefaultArgsViaActualFakeOverride/disabledFeature.kt");
