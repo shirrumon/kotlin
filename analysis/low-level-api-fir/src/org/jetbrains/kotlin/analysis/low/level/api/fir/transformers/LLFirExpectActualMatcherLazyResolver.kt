ba37acee359b26883331ae982fda17f3beea6659
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-11-20 17:40:06 +0100

    [FIR] Fix disappeared ACTUAL_MISSING
    ^KT-59887 Fixed
    
    Review: https://jetbrains.team/p/kt/reviews/13244

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/transformers/LLFirExpectActualMatcherLazyResolver.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/transformers/LLFirExpectActualMatcherLazyResolver.kt
@@ -8,10 +8,12 @@ package org.jetbrains.kotlin.analysis.low.level.api.fir.transformers
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.targets.LLFirResolveTarget
 import org.jetbrains.kotlin.analysis.low.level.api.fir.file.builder.LLFirLockProvider
 import org.jetbrains.kotlin.analysis.low.level.api.fir.util.checkExpectForActualIsResolved
+import org.jetbrains.kotlin.config.LanguageFeature
 import org.jetbrains.kotlin.fir.FirElementWithResolveState
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.*
 import org.jetbrains.kotlin.fir.expressions.FirStatement
+import org.jetbrains.kotlin.fir.languageVersionSettings
 import org.jetbrains.kotlin.fir.resolve.ScopeSession
 import org.jetbrains.kotlin.fir.resolve.transformers.body.resolve.FirResolveContextCollector
 import org.jetbrains.kotlin.fir.resolve.transformers.mpp.FirExpectActualMatcherTransformer
@@ -30,8 +32,12 @@ internal object LLFirExpectActualMatcherLazyResolver : LLFirLazyResolver(FirReso
     }
 
     override fun phaseSpecificCheckIsResolved(target: FirElementWithResolveState) {
-        if (target !is FirMemberDeclaration || !target.canHaveExpectCounterPart()) return
-        checkExpectForActualIsResolved(target)
+        if (target.moduleData.session.languageVersionSettings.supportsFeature(LanguageFeature.MultiPlatformProjects) &&
+            target is FirMemberDeclaration &&
+            target.canHaveExpectCounterPart()
+        ) {
+            checkExpectForActualIsResolved(target)
+        }
     }
 }
 
@@ -41,6 +47,8 @@ private class LLFirExpectActualMatchingTargetResolver(
     session: FirSession,
     scopeSession: ScopeSession,
 ) : LLFirTargetResolver(target, lockProvider, FirResolvePhase.EXPECT_ACTUAL_MATCHING) {
+    private val enabled = session.languageVersionSettings.supportsFeature(LanguageFeature.MultiPlatformProjects)
+
     @Deprecated("Should never be called directly, only for override purposes, please use withRegularClass", level = DeprecationLevel.ERROR)
     override fun withRegularClassImpl(firClass: FirRegularClass, action: () -> Unit) {
         // Resolve outer classes before resolving inner declarations. It's the requirement of FirExpectActualResolver
@@ -57,9 +65,9 @@ private class LLFirExpectActualMatchingTargetResolver(
     }
 
     override fun doLazyResolveUnderLock(target: FirElementWithResolveState) {
-        if (target !is FirMemberDeclaration) return
-        if (!target.canHaveExpectCounterPart()) return
-        transformer.transformMemberDeclaration(target)
+        if (enabled && target is FirMemberDeclaration && target.canHaveExpectCounterPart()) {
+            transformer.transformMemberDeclaration(target)
+        }
     }
 }
 
