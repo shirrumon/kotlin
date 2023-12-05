fa4f0a713b49064f885e69c79be4089530fbcffb
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2023-12-05 17:38:14 +0100

    [Gradle, JS] Fix test warning

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/BuildOptions.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/BuildOptions.kt
@@ -160,7 +160,7 @@ data class BuildOptions(
             jsOptions.incrementalJs?.let { arguments.add("-Pkotlin.incremental.js=$it") }
             jsOptions.incrementalJsKlib?.let { arguments.add("-Pkotlin.incremental.js.klib=$it") }
             jsOptions.incrementalJsIr?.let { arguments.add("-Pkotlin.incremental.js.ir=$it") }
-            jsOptions.yarn?.let { arguments.add("-Pkotlin.js.yarn=$it") }
+            jsOptions.yarn.let { arguments.add("-Pkotlin.js.yarn=$it") }
         }
 
         if (androidVersion != null) {
