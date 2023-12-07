12106d7c5533d15d21c0e89845dc917997e656aa
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2023-12-07 17:43:33 +0100

    [Wasm] Add initialization flag

--- compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/K2JSCompilerArguments.kt
+++ compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/K2JSCompilerArguments.kt
@@ -661,6 +661,16 @@ In combination with '-meta-info', this generates both IR and pre-IR versions of 
         }
 
     @Argument(
+        value = "-Xwasm-disable-initialization",
+        description = "Disables the automatic initialization of the WebAssembly module. When this option is active, the exported function '_initialize' must be called manually prior to calling any other exported functions."
+    )
+    var wasmDisableInitialization = false
+        set(value) {
+            checkFrozen()
+            field = value
+        }
+
+    @Argument(
         value = "-Xforce-deprecated-legacy-compiler-usage",
         description = "This flag is used only for our inner infrastructure. It will soon be removed, so it's no longer safe to use."
     )
