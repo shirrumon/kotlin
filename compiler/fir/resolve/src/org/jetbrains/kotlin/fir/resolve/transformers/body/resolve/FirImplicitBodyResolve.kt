858d6f65812b9d5ed084588b108b776ed1e44ee6
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-12-12 08:33:08 +0100

    Revert "[LL FIR] LLFirImplicitTypesLazyResolver: add cancellation logic"
    This reverts commit 72b44562acce7c5799b2b736339351d28af419bd.

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirImplicitBodyResolve.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirImplicitBodyResolve.kt
@@ -210,8 +210,6 @@ open class ReturnTypeCalculatorWithJump(
     var outerTowerDataContexts: FirRegularTowerDataContexts? = null
 
     override fun tryCalculateReturnTypeOrNull(declaration: FirCallableDeclaration): FirResolvedTypeRef {
-        implicitBodyResolveComputationSession.checkIsResolutionOutdated()
-
         // Local declarations must be handled by `ReturnTypeCalculatorForFullBodyResolve` to avoid resolution cycles in LL FIR.
         if (declaration.visibility == Visibilities.Local) {
             return ReturnTypeCalculatorForFullBodyResolve.Default.tryCalculateReturnType(declaration)
@@ -404,8 +402,6 @@ open class ImplicitBodyResolveComputationSession {
         }
         return implicitBodyResolveStatusMap[symbol] ?: ImplicitBodyResolveComputationStatus.NotComputed
     }
-
-    open fun checkIsResolutionOutdated() {}
 
     protected open fun <D : FirCallableDeclaration> executeTransformation(symbol: FirCallableSymbol<*>, transformation: () -> D): D {
         return transformation()
