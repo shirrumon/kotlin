f3664f19734c7e02ee78bb723003c3ff411ea438
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2024-01-19 16:18:10 +0100

    K2/tests: add a similar to containsAndOverload test about charAt / get
    Related to KT-64846

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
@@ -22474,6 +22474,12 @@ public class FirLightTreeOldFrontendDiagnosticsTestGenerated extends AbstractFir
                 }
 
                 @Test
+                @TestMetadata("charAtAndOverload.kt")
+                public void testCharAtAndOverload() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/j+k/collectionOverrides/charAtAndOverload.kt");
+                }
+
+                @Test
                 @TestMetadata("charBuffer.kt")
                 public void testCharBuffer() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/j+k/collectionOverrides/charBuffer.kt");
