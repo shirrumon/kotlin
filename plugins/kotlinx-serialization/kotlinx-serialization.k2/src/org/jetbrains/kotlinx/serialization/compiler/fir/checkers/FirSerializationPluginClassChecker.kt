2abfc43ec755f23d6c271a6b61d896b30b9e4565
Leonid Startsev <leonid.startsev@jetbrains.com>
2024-01-19 18:03:43 +0000

    fixup! [KxSerialization] Added inspections on custom serializer parameters

--- plugins/kotlinx-serialization/kotlinx-serialization.k2/src/org/jetbrains/kotlinx/serialization/compiler/fir/checkers/FirSerializationPluginClassChecker.kt
+++ plugins/kotlinx-serialization/kotlinx-serialization.k2/src/org/jetbrains/kotlinx/serialization/compiler/fir/checkers/FirSerializationPluginClassChecker.kt
@@ -648,7 +648,7 @@ object FirSerializationPluginClassChecker : FirClassChecker() {
 
         if ( // for external serializer, the verification will be carried out at the definition
             !isExternalSerializer
-            // it is allowed that parameters con not be passed in regular serializers at all
+            // it is allowed that parameters are not passed in regular serializers at all
             && primaryConstructor.valueParameterSymbols.isNotEmpty()
             // if the parameters are still specified, then their number must match in the serializable class and constructor
             && serializerForType.typeArguments.size != primaryConstructor.valueParameterSymbols.size
