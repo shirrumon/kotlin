b8f2607e38d2b1b585efb050432e2a6c4769c3f8
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2024-01-24 16:50:47 +0100

    K2/Java: implement platform-dependent function filtering in JvmMappedScope
    We drop Kotlin function 'remove' or 'getOrDefault' from JvmMappedScope,
    if it has platform-dependent annotation, and the bound Java class scope
    does not contain a function with the same signature.
    
    #KT-57268 Fixed

--- compiler/testData/ir/irText/firProblems/kt43342.kt
+++ compiler/testData/ir/irText/firProblems/kt43342.kt
@@ -1,8 +1,6 @@
 // WITH_STDLIB
 // IGNORE_BACKEND_K1: JS_IR, JS_IR_ES6
 // ^ Map has js specific methods
-// IGNORE_BACKEND_K2: JS_IR
-// IGNORE_BACKEND_K2: NATIVE
 
 open class ControlFlowInfo<K, V>(val map: Map<K, V>): Map<K, V> by map
 
