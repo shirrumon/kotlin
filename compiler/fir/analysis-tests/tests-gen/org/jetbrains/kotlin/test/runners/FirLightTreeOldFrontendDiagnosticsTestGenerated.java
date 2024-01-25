958cfe87e70798f8dc238ada4e34e24bcb0d0f4d
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2024-01-25 18:46:45 +0100

    [Tests] Add a test on smartcasts on properties from dependsOn modules
    Behaviour is undesired, will be fixed in the next commit

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
@@ -34394,6 +34394,12 @@ public class FirLightTreeOldFrontendDiagnosticsTestGenerated extends AbstractFir
                 }
 
                 @Test
+                @TestMetadata("propertyFromDependsOnModule.kt")
+                public void testPropertyFromDependsOnModule() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/smartCasts/variables/propertyFromDependsOnModule.kt");
+                }
+
+                @Test
                 @TestMetadata("propertyNotNeeded.kt")
                 public void testPropertyNotNeeded() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/smartCasts/variables/propertyNotNeeded.kt");
