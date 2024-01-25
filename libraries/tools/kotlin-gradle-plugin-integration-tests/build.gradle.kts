d53ca102cc99f208e5a6abb05555db7e2c5d9c1b
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-01-25 10:36:42 +0100

    [Gradle] Bump max Gradle version in tests to 8.5
    ^KT-62540 Verification Pending

--- libraries/tools/kotlin-gradle-plugin-integration-tests/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/build.gradle.kts
@@ -218,7 +218,7 @@ fun Test.includeTestsWithPattern(include: Boolean, patterns: (MutableSet<String>
 }
 
 fun Test.advanceGradleVersion() {
-    val gradleVersionForTests = "8.4"
+    val gradleVersionForTests = "8.5"
     systemProperty("kotlin.gradle.version.for.tests", gradleVersionForTests)
 }
 
